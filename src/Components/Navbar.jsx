5aeb27b110b792a9381fb675cbe20162d8e10720
RAJ <singhrajtilak64@gmail.com>
2024-07-10 21:42:22 +0530

    Asdaf

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,10 +8,10 @@ function Navbar() {
         </div>
         <div className="menu flex w-full justify-between items-center my-4">
             <div className='w-3/12'>
-            <img src={".../src/assets/Navbar/hamburger.png"} width={"40px"} height={"40px"} alt="" />
+            <img src={"./assets/Navbar/hamburger.png"} width={"40px"} height={"40px"} alt="" />
             </div>
             
-            <img src={"/src/assets/Navbar/Luffy Store.png"} width={"180px"} height={"70px"} alt="" />
+            <img src={"/assets/Navbar/Luffy Store.png"} width={"180px"} height={"70px"} alt="" />
             <div className='menu-function flex w-3/12 justify-evenly items-center'>
                 <img src={"../src/assets/Navbar/search.png"} width={"35px"} height={"35px"} alt="" />
                 <img src={"../src/assets/Navbar/love.png"} width={"35px"} height={"35px"}  alt="" />
