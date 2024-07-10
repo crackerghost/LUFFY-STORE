bcbfb18fbd2048781277d9519f7d622205409e09
RAJ <singhrajtilak64@gmail.com>
2024-07-10 21:55:48 +0530

    jukg

--- src/Components/Home/Slider.jsx
+++ src/Components/Home/Slider.jsx
@@ -4,13 +4,16 @@ function Slider() {
   return (
     <div className='slider-container'>
         <div className="action-btn">
-            <ul className='flex w-full justify-evenly h-10'>
+          <div className='h-[10vh]'>
+          <ul className='flex w-full justify-evenly'>
                 <li>HOME</li>
                 <li>NEW ARRIVALS</li>
                 <li>BEST SELLERS</li>
                 <li>HOODIE</li>
                 <li>ANIME</li>
             </ul>
+          </div>
+           
             <div className="slider">
                 <div className='slider-inner rounded-2xl border-black-2 bg-[--graytrans--]  w-[400px] h-[260px] backdrop-blur-lg text-white'>
                    
