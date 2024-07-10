aa61fcf3bd7fba01e52e59c6d8dc63d1a68cb9d7
RAJ <singhrajtilak64@gmail.com>
2024-07-10 21:37:55 +0530

    Asdaf

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
             <img src={".../src/assets/Navbar/hamburger.png"} width={"40px"} height={"40px"} alt="" />
             </div>
             
-            <img src={"./src/assets/Navbar/Luffy Store.png"} width={"180px"} height={"70px"} alt="" />
+            <img src={"/src/assets/Navbar/Luffy Store.png"} width={"180px"} height={"70px"} alt="" />
             <div className='menu-function flex w-3/12 justify-evenly items-center'>
                 <img src={"../src/assets/Navbar/search.png"} width={"35px"} height={"35px"} alt="" />
                 <img src={"../src/assets/Navbar/love.png"} width={"35px"} height={"35px"}  alt="" />
