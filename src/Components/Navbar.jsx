b7982f47a54b3b59182afc444a26e64739c94fb0
RAJ <singhrajtilak64@gmail.com>
2024-07-10 21:51:42 +0530

    asfsa

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,14 +8,14 @@ function Navbar() {
         </div>
         <div className="menu flex w-full justify-between items-center my-4">
             <div className='w-3/12'>
-            <img src={"./assets/Navbar/hamburger.png"} width={"40px"} height={"40px"} alt="" />
+            <img src={"/assets/Navbar/hamburger.png"} width={"40px"} height={"40px"} alt="" />
             </div>
             
             <img src={"/assets/Navbar/Luffy Store.png"} width={"180px"} height={"70px"} alt="" />
             <div className='menu-function flex w-3/12 justify-evenly items-center'>
-                <img src={"../src/assets/Navbar/search.png"} width={"35px"} height={"35px"} alt="" />
-                <img src={"../src/assets/Navbar/love.png"} width={"35px"} height={"35px"}  alt="" />
-                <img src={"../src/assets/Navbar/cart.png"}width={"35px"} height={"35px"}  alt="" />
+                <img src={"/assets/Navbar/search.png"} width={"35px"} height={"35px"} alt="" />
+                <img src={"/assets/Navbar/love.png"} width={"35px"} height={"35px"}  alt="" />
+                <img src={"/src/assets/Navbar/cart.png"}width={"35px"} height={"35px"}  alt="" />
             </div>
 
         </div>
