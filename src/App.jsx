b14dc3d8bbcd01c612c77327effb31d9103ae8db
RAJ <singhrajtilak64@gmail.com>
2024-07-11 16:46:34 +0530

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 
 import './App.css'
@@ -6,9 +6,32 @@ import Home from './Components/Home/Home'
 
 function App() {
 
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(()=>{
+       const loaded = ()=>{
+          setIsLoaded(true);
+
+          
+       }
+       window.addEventListener('load', loaded)
+     
+   
+       return () => {
+        window.removeEventListener('load', loaded);
+      };
+  },[])
+
+  if (!isLoaded) {
+    return (<div className='h-[100vh] w-[100vw] flex flex-col justify-center items-center'>
+
+      <img width={"150px"} src="/assets/loader.gif" alt="" />
+      <p className='font-stylescript font-bold text-3xl'>Loading...</p>
+    </div>)
+  }
 
   return (
-    
+
     <BrowserRouter>
     <Routes>
     <Route path='/' element={<Home/>}/>
